60db86a9045b97a7990201ef8ae55ebde8dd6fac
Iann Molendolff <iannmolendolff@gmail.com>
2024-06-25 03:55:53 -0300

    Primeiro commit - criação do projeto

---
+++ src/main/java/com/iannmolendolff/mondodb_springboot/MondodbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.iannmolendolff.mondodb_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondodbSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondodbSpringbootApplication.class, args);
+	}
+
+}
