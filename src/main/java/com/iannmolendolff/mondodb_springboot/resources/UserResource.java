85295de07b4167e6bd57743cda03806a5a5a9fc5
Iann Molendolff <iannmolendolff@gmail.com>
2024-06-26 20:44:36 -0300

    Usando DTO para retornar usuários

--- src/main/java/com/iannmolendolff/mondodb_springboot/resources/UserResource.java
+++ src/main/java/com/iannmolendolff/mondodb_springboot/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.iannmolendolff.mondodb_springboot.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iannmolendolff.mondodb_springboot.domain.User;
+import com.iannmolendolff.mondodb_springboot.dto.UserDTO;
 import com.iannmolendolff.mondodb_springboot.services.UserService;
 
 @RestController
@@ -22,8 +22,9 @@ public class UserResource {
 	
 	// or @GetMapping
 	@RequestMapping(method= RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
