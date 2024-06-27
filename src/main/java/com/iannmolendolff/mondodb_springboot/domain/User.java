c57352aabd8653aa987a685295cac82b1ae7854c
Iann Molendolff <iannmolendolff@gmail.com>
2024-06-27 01:22:26 -0300

    Referenciando os posts do usuário

--- src/main/java/com/iannmolendolff/mondodb_springboot/domain/User.java
+++ src/main/java/com/iannmolendolff/mondodb_springboot/domain/User.java
@@ -1,9 +1,12 @@
 package com.iannmolendolff.mondodb_springboot.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts= new ArrayList<>();
 	
 	public User() {
 		
@@ -51,11 +57,19 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -67,6 +81,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 }
